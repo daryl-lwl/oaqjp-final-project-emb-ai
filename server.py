8cc47693075037d41eb0e9d7d07b6804f97af77f
daryl-lwl <daryl.leewl@gmail.com>
2024-12-15 10:15:58 -0500

    Fourth Commit

--- server.py
+++ server.py
@@ -3,14 +3,18 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
-@app.route("/emotionDetection")
+@app.route("/emotionDetector")
 def emo_detector():
     text_to_analyze = request.args.get('textToAnalyze')
-    print(text_to_analyze)
     response = emotion_detector(text_to_analyze)
+    various_emotions = str(response[:-1]).replace('{',' ')
+    various_emotions = various_emotions.replace('(',' ')
+    various_emotions = various_emotions.replace('}',' ')
+    various_emotions = various_emotions.replace(')',' ')
+    various_emotions = various_emotions[:-3]
     dominant_response = response[-1].split()
 
-    return "Testing"
+    return "For the given statement, the system response is {}. The dominant emotion is {}.".format(various_emotions, dominant_response[-1])
 
 @app.route("/")
 def render_index_page():
